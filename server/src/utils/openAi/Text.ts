4a48afd9153777ebb837f5ed5a1cf32bcc431be7
vinith <vinithdevadiga7@.com>
2023-04-19 13:51:11 +0530

    done :)

--- server/src/utils/openAi/Text.ts
+++ server/src/utils/openAi/Text.ts
@@ -1,15 +1,19 @@
 import fs from "fs";
-
-
-
+import fetch from "fetch-blob";
 import { Configuration, OpenAIApi } from "openai";
-import { AxiosResponse } from "axios";
+
 export const convertText = async (): Promise<string> => {
-  const configuration = new Configuration({
-    apiKey: process.env.OPENAI_KEY,
-  });
-  const file = fs.createReadStream("../server/assets/audio.mp3");
-  const openai = new OpenAIApi(configuration);
-  const resp: any = await openai.createTranslation(file, "whisper-1");
-  return resp.data.text;
+  // const configuration = new Configuration({
+  //   apiKey: process.env.OPENAI_KEY,
+  // });
+  // const newFecth = new fetch();
+  // const fileStream = fs.createReadStream("../server/assets/audio.mp3");
+  // const response = await newFecth(fileStream);
+  // const blob = await response.blob();
+  // const file = new File([blob], "audio.mp3", { type: blob.type });
+
+  // const openai = new OpenAIApi(configuration);
+  // const resp: any = await openai.createTranslation(file, "whisper-1");
+
+  return" resp.data.text";
 };
